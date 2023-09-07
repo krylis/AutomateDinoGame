490cec1822f327485b019c207c72e533c8e5ef12
krylis <cmcshann@gmail.com>
2023-09-07 11:52:19 -0700

    Reworked to take a screenshot of a specific area of the screen and then get the color of the center pixel to determine if there is an obstacle approaching the dinosaur.

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
-import webbrowser
 import pyautogui
+import keyboard
+import webbrowser
 import time
 
-webbrowser.open('https://elgoog.im/t-rex/')
+webbrowser.open("https://elgoog.im/dinosaur-game/")
+time.sleep(1)
+keyboard.press('space')
+time.sleep(3)
 
-time.sleep(2)
+while not keyboard.is_pressed('q'):
+    low_obstacle_area = pyautogui.screenshot(region=(250, 460, 100, 100))
+    bg_color = low_obstacle_area.getpixel((50, 50))
+    if bg_color != (255, 255, 255):
+        keyboard.press('space')
 
-pyautogui.press('space')
+    high_obstacle_area = pyautogui.screenshot(region=(300, 400, 100, 100))
+    bg_color2 = high_obstacle_area.getpixel((50, 50))
+    if bg_color2 != (255, 255, 255):
+        keyboard.press('down')
 
-im = pyautogui.screenshot()
